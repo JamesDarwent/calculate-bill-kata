da3b061bd2d72f6dfd90f250ee02aa9e08070366
James Darwent <jamesdarwent_88@hotmail.co.uk>
2020-12-15 12:17:05 +0000

    Calculate bbill kata completed

---
+++ index.js
@@ -0,0 +1,9 @@
+const calculateBill = (price, vat, tip) => {
+    if(typeof (price, vat, tip) === "number") {
+        let amount = price + (price * vat / 100) + tip;
+        return "£" + amount;
+    } else {
+        return "error"
+    }
+};
+module.exports = calculateBill;
